054fa5df878bc4914a7f1ceea6ed586572c385ba
Darcy Junior <darcydeoliveirajunior@gmail.com>
2025-10-19 21:08:09 -0300

    Retorno pelo bot

--- api/webhook.js
+++ api/webhook.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(200).send('OK');
@@ -5,7 +7,21 @@ export default async function handler(req, res) {
 
   const update = req.body;
 
-  console.log('Mensagem recebida do Telegram:', update);
+  // Verifica se é uma mensagem comum
+  if (update.message) {
+    const chatId = update.message.chat.id;
+    const text = update.message.text;
+
+    // Responde de volta pro Telegram
+    await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: `Você disse: ${text}`
+      })
+    });
+  }
 
   // Apenas responde OK para o Telegram
   return res.status(200).send('OK');
